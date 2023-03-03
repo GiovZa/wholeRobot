718a4b19c8f94d6f9604e46219b404bbec43eeb3
GiovZa <92822460+GiovZa@users.noreply.github.com>
2023-03-03 00:31:51 -0600

    Update move_base_client.py

--- gen2bot/scripts/move_base_client.py
+++ gen2bot/scripts/move_base_client.py
@@ -9,6 +9,9 @@ import rospy
 # Brings in the SimpleActionClient
 import actionlib
 
+from std_msgs.msg import String
+from std_msgs.msg import Int8
+
 # Brings in the .action file and messages used by the move base action
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 
@@ -16,11 +19,11 @@ import tf2_ros
 import geometry_msgs.msg
 import tf2_msgs.msg
 
-def movebase_client():
+def movebase_client(xPos, yPos):
 
    # Create an action client called "move_base" with action definition file "MoveBaseAction"
     client = actionlib.SimpleActionClient('move_base',MoveBaseAction)
- 
+
    # Waits until the action server has started up and started listening for goals.
     client.wait_for_server()
 
@@ -28,54 +31,36 @@ def movebase_client():
     tfBuffer = tf2_ros.Buffer()
     listener = tf2_ros.TransformListener(tfBuffer)
 
-    rate = rospy.Rate(10.0)
-    #new transform for qr code to position we want
-    #Does this need to be updated or can it just be run once? if it needs to be broadcasted should we just put it in the launch file?
-    while not rospy.is_shutdown():
-       rospy.sleep(0.1)
-       t = geometry_msgs.msg.TransformStamped()
-       t.header.frame_id = "object_22"
-       t.header.stamp = rospy.Time.now()
-       t.child_frame_id = "goal_tf"
-       t.transform.translation.x = 2.0
-       t.transform.translation.y = 2.0
-       t.transform.translation.z = 0.0
-   
-       t.transform.rotation.x = 0.0
-       t.transform.rotation.y = 0.0
-       t.transform.rotation.z = 0.0
-       t.transform.rotation.w = 1.0
-      
-       tfm = tf2_msgs.msg.TFMessage([t])
-   
-    #new transform for qr code to position we want
-    #Does this need to be updated or can it just be run once? if it needs to be broadcasted should we just put it in the launch file?
-    while not rospy.is_shutdown():
-       rospy.sleep(0.1)
-       t = geometry_msgs.msg.TransformStamped()
-       t.header.frame_id = "object_22"
-       t.header.stamp = rospy.Time.now()
-       t.child_frame_id = "goal_tf"
-       t.transform.translation.x = 3.0
-       t.transform.translation.y = 1.0
-       t.transform.translation.z = 0.0
-   
-       t.transform.rotation.x = 0.0
-       t.transform.rotation.y = 0.0
-       t.transform.rotation.z = 0.0
-       t.transform.rotation.w = 1.0
-      
-       tfm = tf2_msgs.msg.TFMessage([t])
+    # Creating a temporary static tf publisher between QR code and goal
+    broadcaster = tf2_ros.StaticTransformBroadcaster()
+    static_transformStamped = geometry_msgs.msg.TransformStamped()
+    static_transformStamped.header.stamp = rospy.Time.now()
+    static_transformStamped.header.frame_id = "object_22"
+    static_transformStamped.child_frame_id = "goal_tf"
+    static_transformStamped.transform.translation.x = xPos
+    static_transformStamped.transform.translation.y = yPos
+    static_transformStamped.transform.translation.z = 0.0
+    quat = tf.transformations.quaternion_from_euler(0, 0, 0)
+    static_transformStamped.transform.rotation.x = quat[0]
+    static_transformStamped.transform.rotation.y = quat[1]
+    static_transformStamped.transform.rotation.z = quat[2]
+    static_transformStamped.transform.rotation.w = quat[3]
+    broadcaster.sendTransform(static_transformStamped)
+    
+    tfm = tf2_msgs.msg.TFMessage([t])
+
+    rate = rospy.Rate(10.0)   
+
    
     while not rospy.is_shutdown():
+        rospy.sleep(0.1)
      try:
-         # Gets position from digPose to map, header has to be 'map' because that's where move_base sits
-         trans = tfBuffer.lookup_transform('object_22', 'goal_tf', rospy.Time())
+        # With the transform between QR code and goal_tf, we can now get transform between map and goal_tf
+        # to get absolute position of goal_tf
+         trans = tfBuffer.lookup_transform('map', 'goal_tf', rospy.Time())
      except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
          rate.sleep()
          continue
-
- 
  
    # Creates a new goal with the MoveBaseGoal constructor
     goal = MoveBaseGoal()
@@ -84,11 +69,8 @@ def movebase_client():
     goal.target_pose.header.frame_id = "map"
     goal.target_pose.header.stamp = rospy.Time.now()
    
-   # Gets positional coordinates from map frame to digPose child and inputs it into the goal pose
-   # Need to reformat so we add the position between qr code and digPose here, rather than create a
-   # tf static broadcaster always outputting digPose position unnecessarily 
-    goal.target_pose.pose.position.x = tfm.transform.translation.x - trans.transform.translation.x
-    goal.target_pose.pose.position.y = tfm.transform.translation.y - trans.transform.translation.y
+    goal.target_pose.pose.position.x = trans.transform.translation.x
+    goal.target_pose.pose.position.y = trans.transform.translation.y
     goal.target_pose.pose.position.z = 0
 
    # No rotation of the mobile base frame w.r.t. map frame
@@ -106,12 +88,27 @@ def movebase_client():
     # Result of executing the action
         return client.get_result()   
 
-if __name__ == '__main__':
+def callback(data):
+    if data.data == 7:
+        xPos = 1.0
+        yPos = 0.0
+    elif data.data == 6:
+        xPos = 3.0
+        yPos = 1.0
+    else:
+        print("No move_base values given: ")
+        return
     try:
-       # Initializes a rospy node to let the SimpleActionClient publish and subscribe
-        rospy.init_node('movebase_client_py')
-        result = movebase_client()
+        result = movebase_client(xPos, yPos)
         if result:
             rospy.loginfo("Goal execution done!")
     except rospy.ROSInterruptException:
         rospy.loginfo("Navigation test finished.")
+
+if __name__ == '__main__':
+    rospy.init_node('move_base_client_process_manager')
+    
+    # Publisher is from notDTAuto.cpp
+    rospy.Subscriber('robot_status', Int8, callback)
+
+    rospy.spin()
